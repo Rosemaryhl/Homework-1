195c3ac79b6bde530c6c10d93ea7f6c8aa8fde85
Rosemary Hoffman <123026684+Rosemaryhl@users.noreply.github.com>
2023-02-04 15:05:19 -0500

    Add files via upload

---
+++ Homework 1.py
@@ -0,0 +1,66 @@
+>>> #Rosemary Hoffman
+>>> #Homework 1
+>>> #Problem 1
+>>> type ("Rosewater")
+<class 'str'>
+>>> type (0.001)
+<class 'float'>
+>>> type (37)
+<class 'int'>
+>>> type (False)
+<class 'bool'>
+>>> #Problem 2
+>>> #The addition symbol can be used to add strings together for example:
+>>> print ("Happy" + "day")
+Happyday
+>>> first = "Happy"
+>>> second = "day"
+>>> first + second
+'Happyday'
+>>> print (first + second)
+Happyday
+>>> print ("happy, day")
+happy, day
+>>> print ("today", "is", "a", "good", "day")
+today is a good day
+>>> #The addition operator adds the strings whereas the comma operator keeps them separate.
+>>> #Problem 3
+>>> #There are built-in functions that you can use to turn your argument into the function's type.
+>>> int (3.5)
+3
+>>> #the interger functions turns the argument into an integer.
+>>> float (4)
+4.0
+>>> #the float function turns the argument into a decimal (known as float).
+>>> str ("wednesday")
+'wednesday'
+>>> #the string function turns the argument into a string.
+>>> str ("4")
+'4'
+>>> bool (1)
+True
+>>> bool (0)
+False
+>>> #the bool function turns the argument into True or False.
+>>> #Problem 4
+>>> print ("life has loveliness to sell\nAll beautiful and splendid things\nBlue waves whitened on a cliff\n Soaring fire that sways and sings\nAnd children's faces looking up\n Holding wonder like a cup")
+life has loveliness to sell
+All beautiful and splendid things
+Blue waves whitened on a cliff
+ Soaring fire that sways and sings
+And children's faces looking up
+ Holding wonder like a cup
+>>> #By using back-slash n (\n) you start a new line.
+...  
+>>> #Problem 5
+...  
+>>> flour = 2
+>>> milk = 1
+>>> egg = 4
+>>> oil = 0.4
+>>> vanilla = 0.012
+>>> 2 * flour + 0.5 * (milk - egg + vanilla) + oil**2
+2.6660000000000004
+>>> cake = 2 * flour + 0.5 * (milk - egg + vanilla) + oil**2
+>>> print (cake)
+2.6660000000000004
